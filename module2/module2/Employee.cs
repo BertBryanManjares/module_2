f42420702b9fd627e8298c82696aebbd4eaeb89c
BertBryanManjares <bertbryanmanjares92@gmail.com>
2024-11-23 19:47:08 +0800

    Add files via upload

---
+++ module2/module2/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Module_2
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Position: {Position}, Salary: {Salary:C}";
+        }
+    }
+}
